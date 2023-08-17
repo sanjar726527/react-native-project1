f8a0d5361a004391264413cd004d55b725abdde4
Sanjar <103542978+sanjar726527@users.noreply.github.com>
2023-08-17 09:30:43 +0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList,
+  Pressable,
+} from "react-native";
+
+const data = [
+  "List1",
+  "List2",
+  "List3",
+  "List4",
+  "List5",
+  "List6",
+  "List7",
+  "List8",
+  "List9",
+  "List10",
+];
 
 export default function App() {
+  const [text, setText] = useState("");
+  const [items, setItems] = useState(data);
+
+  function onchangeFunction(e) {
+    setText(e);
+  }
+
+  function onClickButton() {
+    setItems((current) => [...current, text]);
+    setText("");
+  }
+
+  function handlePressRemove(e) {
+    console.log(e);
+    setItems((prevList) => prevList.filter((item, index) => index !== e));
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          onChangeText={onchangeFunction}
+          placeholder="Your skills"
+          placeholderTextColor={"#000"}
+          value={text}
+        />
+        <Button title="Add Skills" onPress={onClickButton} />
+      </View>
+
+      <View
+        style={{
+          paddingTop: 30,
+          flex: 3,
+        }}
+      >
+        <FlatList
+          data={items}
+          renderItem={(item) => (
+            <Pressable onPress={() => handlePressRemove(item.index)}>
+              <Text style={styles.listText}>{item.item}</Text>
+            </Pressable>
+          )}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 60,
+    paddingHorizontal: 15,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#a1a1a1",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingBottom: 30,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#5190e4",
+    flex: 1,
+    paddingLeft: 10,
+  },
+  listText: {
+    borderRadius: 1.5,
+    padding: 20,
+    marginBottom: 10,
+    color: "#fff",
+    fontSize: 20,
+    borderColor: "#3e3e",
+    backgroundColor: "#4e88f3",
   },
 });
